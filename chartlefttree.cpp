8eb551c031ac78aa90dfc46fedc202667c0cd365
zhaoshuai3 <1115870494@qq.com>
2019-09-18 18:26:48 +0800

    修改颜色

--- chartlefttree.cpp
+++ chartlefttree.cpp
@@ -17,8 +17,5 @@ ChartLeftTree::ChartLeftTree(QTreeWidget *parent) : QTreeWidget(parent)
         item->setIcon(0,QIcon(CHART_ICON_PATH));
     }
     this->setHeaderHidden(true);
-
-
-
-
+    this->setStyleSheet("QTreeView{background-color:rgb(131,139,139)} QTreeView::branch::hover{background-color:rgb(224,238,238);} QTreeView::item::hover{background-color:rgb(224,238,238);} QTreeView::branch::selected{background-color:green;} QTreeView::item::selected{background-color:green;} ");
 }
